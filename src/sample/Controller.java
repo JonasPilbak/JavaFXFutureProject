9f6086c81aadcff3f4f1290e669fb3ae5e809556
jonas Henriksen <jonas.pilbak@hotmail.com>
2020-02-04 12:23:05 +0100

    Rdy to go!!!!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
 public class Controller {
+
+    @FXML
+    private Pane mainCourse;
+
+    @FXML
+    private Pane burgers;
+
+    @FXML
+    private Pane sideDish;
+
+    @FXML
+    private Pane dippings;
+
+
+    @FXML
+    private Button corsses;
+
+    @FXML
+    private Button salats;
+
+    @FXML
+    private Button dishes;
+
+    @FXML
+    private Button dips;
+
+
+
+
 }
